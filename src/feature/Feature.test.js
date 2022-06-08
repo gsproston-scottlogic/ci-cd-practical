ca23281eac329b36d21027237bdd7869d1ba4a9f
George Sproston <gsproston@scottlogic.com>
2022-06-08 16:20:38 +0100

    Test now passes

--- src/feature/Feature.test.js
+++ src/feature/Feature.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Feature from './Feature';
 
-test('renders learn react link', () => {
+test('renders Hello world', () => {
   render(<Feature />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Hello world.");
   expect(linkElement).toBeInTheDocument();
 });
